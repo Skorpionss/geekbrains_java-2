22c6a7f771a55bb0a631beb8b451ec2d9563708f
Maxim <the_prince@sibmail.com>
2021-03-15 04:06:55 +0700

    Задание 4 Создать 2 массива: с участниками и препятствиями, и заставьте всех участников пройти набор препятствий.

--- src/lesson_1/Main.java
+++ src/lesson_1/Main.java
@@ -1,12 +1,31 @@
 package lesson_1;
 
+import lesson_1.obstacles.Obstacles;
 import lesson_1.obstacles.Track;
+import lesson_1.obstacles.Wall;
 import lesson_1.participants.Cat;
+import lesson_1.participants.Human;
+import lesson_1.participants.Participants;
+import lesson_1.participants.Robot;
 
 public class Main {
     public static void main(String[] args) {
-        Cat cat = new Cat();
-        Track track = new Track();
-        System.out.println(track.result(cat));
+
+        Obstacles[] obstacles = {
+                new Track(),
+                new Wall()
+        };
+
+        Participants[] participants = {
+                new Cat(),
+                new Human(),
+                new Robot()
+        };
+
+        for (int i = 0; i < participants.length; i++) {
+            for (int j = 0; j < obstacles.length; j++) {
+                System.out.println(obstacles[j].result(participants[i]));
+            }
+        }
     }
 }
