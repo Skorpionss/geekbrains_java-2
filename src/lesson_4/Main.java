dc80c9d5b950a0f6ae8f7868515fedf81a683319
Maxim <the_prince@sibmail.com>
2021-03-25 23:01:05 +0700

    Задание 1. Создать коллекцию типа List. Наполнить ее значениями и вывести значения в консолько при помощи ее метода forEach.

--- src/lesson_4/Main.java
+++ src/lesson_4/Main.java
@@ -1,7 +1,24 @@
 package lesson_4;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
 public class Main {
     public static void main(String[] args) {
 
+        doTask1();
+    }
+
+    static void doTask1() {
+        List<Integer> listnamber = new ArrayList<>();
+
+        listnamber.add(1);
+        listnamber.add(2);
+
+        listnamber.forEach(integer -> {
+            System.out.println(integer);
+        });
     }
 }
+
