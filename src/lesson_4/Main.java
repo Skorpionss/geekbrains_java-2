29a6943651fa474f19e2795e4205b1d8d127cb2e
Maxim <the_prince@sibmail.com>
2021-03-25 23:28:25 +0700

    Задание 2. Создать утилитарный метод forItem. Метод принимает два параметра: Коллекция Set<String> и консьюмер типа Consumer<String>. Внутри метода проитерироваться по коллекции и для каждого элемента выполнить метод консьюмера accept, который выводит значение элемента в консоль.

--- src/lesson_4/Main.java
+++ src/lesson_4/Main.java
@@ -1,13 +1,12 @@
 package lesson_4;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.function.Consumer;
 
 public class Main {
     public static void main(String[] args) {
 
-        doTask1();
+        doTask2();
     }
 
     static void doTask1() {
@@ -19,6 +18,23 @@ public class Main {
         listnamber.forEach(integer -> {
             System.out.println(integer);
         });
+    }
+
+    static void forItem(Set<String> ss, Consumer<String> cs) {
+        Iterator<String> iterator = ss.iterator();
+        while (iterator.hasNext()) {
+            String val = iterator.next();
+            cs.accept(val);
+        }
+    }
+
+    static void doTask2() {
+        Set<String> mass = new HashSet<>();
+
+        mass.add("stroka 1");
+        mass.add("stroka 2");
+
+        forItem(mass, string -> System.out.println(string));
     }
 }
 
