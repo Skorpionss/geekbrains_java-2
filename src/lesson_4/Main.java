5a66d5578296656aec454171c1b65942339f2179
Maxim <the_prince@sibmail.com>
2021-03-25 23:49:29 +0700

    Задание 3. Создать утилитарный метод doubleUp.

--- src/lesson_4/Main.java
+++ src/lesson_4/Main.java
@@ -2,11 +2,12 @@ package lesson_4;
 
 import java.util.*;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class Main {
     public static void main(String[] args) {
 
-        doTask2();
+        doTask3();
     }
 
     static void doTask1() {
@@ -35,6 +36,14 @@ public class Main {
         mass.add("stroka 2");
 
         forItem(mass, string -> System.out.println(string));
+    }
+
+    static int doubleUp(int i, Supplier<Integer> sl){
+        return i * sl.get();
+    }
+
+    static void doTask3(){
+        System.out.println(doubleUp( 10, () -> 5));
     }
 }
 
