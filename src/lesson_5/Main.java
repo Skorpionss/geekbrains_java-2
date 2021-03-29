c02cfc998eff27765b5c4f61152a606fb2d1a258
Maxim <the_prince@sibmail.com>
2021-03-30 00:06:13 +0700

    Д.р. №5 финал.

--- src/lesson_5/Main.java
+++ src/lesson_5/Main.java
@@ -2,6 +2,8 @@ package lesson_5;
 
 public class Main {
     public static void main(String[] args) {
+        new MultiThreading().NoThreading();
+        new MultiThreading().Threading();
 
     }
 }
