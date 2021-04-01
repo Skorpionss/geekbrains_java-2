8d2d61c436ccc93046cabf4af0631167a624cf5e
Maxim <the_prince@sibmail.com>
2021-04-02 02:06:46 +0700

    Создание 2 потока: для отправки и приема сообщения.

--- src/lesson_6/client/Client.java
+++ src/lesson_6/client/Client.java
@@ -1,13 +1,50 @@
 package lesson_6.client;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
     public Client() {
         try {
             Socket socket = new Socket("127.0.0.1", 8000);
-        } catch (IOException e){
+            System.out.println("Соединение с сервером установленно");
+
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            Scanner scanner = new Scanner(System.in);
+
+            new Thread(() -> {
+                while (true) {
+                    try {
+                        out.writeUTF(scanner.nextLine());
+                    } catch (IOException e) {
+                        System.out.println("Соединение разорванно");
+                        break;
+                    }
+                }
+            }
+
+            ).start();
+
+            new Thread(() -> {
+                while (true) {
+                    try {
+                        String message = in.readUTF();
+                        System.out.println("От сервера: " + message);
+                    } catch (IOException e) {
+                        System.out.println("Соединение разорванно");
+                        break;
+                    }
+                }
+            }
+
+            ).start();
+
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
