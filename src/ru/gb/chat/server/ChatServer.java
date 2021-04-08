9943085dd02a359b33df0da0b85d171ddf6a7735
Maxim <the_prince@sibmail.com>
2021-04-09 06:28:54 +0700

    Создание авторизации пользователя. AuthenticationService - используется в замещении базы данных.

--- src/ru/gb/chat/server/ChatServer.java
+++ src/ru/gb/chat/server/ChatServer.java
@@ -3,16 +3,53 @@ package ru.gb.chat.server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 public class ChatServer {
+    private final AuthenticationService authenticationService;
+    private final Set<ClientHandler> loggedClients;
+
     public ChatServer() throws IOException {
+        authenticationService = new AuthenticationService();
+        loggedClients = new HashSet<>();
         try {
             ServerSocket serverSocket = new ServerSocket(8000);
             Socket socket = serverSocket.accept();
-        } catch (IOException e){
+        } catch (IOException e) {
             throw new ChatServerException("Ошибка", e);
-            }
         }
-
     }
+
+    public AuthenticationService getAuthenticationService() {
+        return authenticationService;
+    }
+
+
+    public void subscribe(ClientHandler clientHandler) {
+        loggedClients.add(clientHandler);
+    }
+
+    public void unsubscribe(ClientHandler clientHandler) {
+        loggedClients.remove(clientHandler);
+    }
+
+    public void broadcast(String message) {
+        for (ClientHandler clientHandler : loggedClients) {
+            clientHandler.sendMessage(message);
+        }
+    }
+
+
+    public boolean isLoggedIn(String name) {
+        Iterator<ClientHandler> iterator = loggedClients.iterator();
+        while (iterator.hasNext()) {
+            ClientHandler client = iterator.next();
+            if (client.getName().equals(name)) ;
+            return true;
+        }
+        return false;
+    }
+}
 
--- src/ru/gb/chat/server/ClientHandler.java
+++ src/ru/gb/chat/server/ClientHandler.java
@@ -4,13 +4,15 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Optional;
 
 public class ClientHandler {
     private Socket socket;
     private DataInputStream in;
     private DataOutputStream out;
+    private String name;
 
-    public ClientHandler(Socket socket) {
+    public ClientHandler(Socket socket, ChatServer chatServer) {
         this.socket = socket;
         try {
             in = new DataInputStream(socket.getInputStream());
@@ -18,6 +20,61 @@ public class ClientHandler {
         } catch (IOException e) {
             throw new ChatServerException("Ошибка в обработчике клиента, передача данных", e);
         }
+
+        doAuthentication(chatServer);
     }
+
+    public String getName() {
+        return name;
+    }
+
+    private void doAuthentication(ChatServer chatServer) {
+        while (true) {
+            try {
+                String massage = in.readUTF();
+                // -auth login password (вид коректного сообщения)
+                if (massage.startsWith("-auth")) {
+                    String[] massagesplit = massage.split(" ");
+                    String login = massagesplit[1];
+                    String password = massagesplit[2];
+
+                    Optional<AuthenticationService.Entry> mayBeCredentials
+                            = chatServer.getAuthenticationService().findEntryByCredentials(login, password);
+
+                    if (mayBeCredentials.isPresent()) {
+                        AuthenticationService.Entry credentials = mayBeCredentials.get();
+                        if (!chatServer.isLoggedIn(credentials.getName())) {
+                            name = credentials.getName();
+                            chatServer.broadcast(String.format("Пользователь с именем %s зашел в чат", name));
+                            chatServer.subscribe(this);
+                            return;
+                        } else {
+                            sendMessage(String.format("Пользователь с именем %s уже залогинен", credentials.getName()));
+                        }
+                    } else {
+                        sendMessage("нет такого логина или пороля");
+                    }
+
+                }
+
+            } catch (IOException e) {
+                throw new ChatServerException("Ошибка в получении сообщения", e);
+            }
+        }
+    }
+
+    public void receiveMessage() {
+
+    }
+
+    public void sendMessage(String message) {
+        try {
+            out.writeUTF(message);
+        } catch (IOException e) {
+            throw new ChatServerException("ошибка передачи сообщения", e);
+        }
+
+    }
+
 }
 
